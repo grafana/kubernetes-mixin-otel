0336066df13c77bd885751c27594bcc70ea53c19
Serena K <serena.kei@grafana.com>
2025-10-24 12:14:17 -0400

    Add docker-build to support tar

--- lgtm.sh
+++ lgtm.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 
+# Build the custom LGTM image with tar support
+echo "Building custom LGTM image with tar support..."
+docker build -t otel-lgtm-with-tar:latest -f DOCKER-BUILDER .
 
+# Create k3d cluster
 k3d cluster create otel-mixin \
     -v "$PWD"/k3d-volume:/k3d-volume
 
+# Import the custom image into k3d
+echo "Importing custom image into k3d cluster..."
+k3d image import otel-lgtm-with-tar:latest -c otel-mixin
+
+# Deploy the LGTM stack
 kubectl apply -f lgtm.yaml
