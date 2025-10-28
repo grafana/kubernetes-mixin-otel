9e90e5d5a5730dcb7904aa8ff89a9d17af54fe4a
Serena K <serena.kei@grafana.com>
2025-10-28 15:07:50 -0400

    working version without datasources

--- README.md
+++ README.md
@@ -1,8 +1,7 @@
 # opentelemetry-mixin
 
 1. Run `lgtm.sh` to start the cluster
-1. Forward to localhost:3001
-    ```
-    kubectl port-forward service/lgtm 3001:3000 4317:4317 4318:4318
-    ```
-1. Go to `localhost:3001/admin/provisioning` to connect the instance to the github repository
+1. Go to `localhost:3000/admin/provisioning` to connect the instance to the github repository
+
+# Delete cluster
+`k3d cluster delete otel-mixin`
--- lgtm.sh
+++ lgtm.sh
@@ -1,16 +1,14 @@
 #!/bin/bash
 
-# Build the custom LGTM image with tar support
-echo "Building custom LGTM image with tar support..."
-docker build -t otel-lgtm-with-tar:latest -f DOCKER-BUILDER .
+k3d cluster delete otel-mixin
 
 # Create k3d cluster
 k3d cluster create otel-mixin \
-    -v "$PWD"/k3d-volume:/k3d-volume
+    -v "$PWD"/k3d-volume:/k3d-volume \
 
 # Import the custom image into k3d
 echo "Importing custom image into k3d cluster..."
-k3d image import otel-lgtm-with-tar:latest -c otel-mixin
+k3d image import grafana/otel-lgtm:latest -c otel-mixin
 
 # Deploy the LGTM stack
 kubectl apply -f lgtm.yaml
