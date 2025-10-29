58b62e5b3a4010147124248d88d2244274d06f8b
Stephen Lang <stephen.lang@grafana.com>
2025-10-29 15:42:31 +0000

    chore: remove custom image command

--- lgtm.sh
+++ lgtm.sh
@@ -10,9 +10,5 @@ k3d cluster create otel-mixin \
     -p "4317:4317@loadbalancer" \
     -p "4318:4318@loadbalancer" \
 
-# Import the custom image into k3d
-echo "Importing custom image into k3d cluster..."
-k3d image import grafana/otel-lgtm:latest -c otel-mixin
-
 # Deploy the LGTM stack
 kubectl apply -f lgtm.yaml
